2c122af74d0ccc4de6d2d690bc659c87757c7ce6
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2022-12-20 09:39:14 -0600

    Noninteractive render of board in Vanilla JS (#4)
    * Just hooks up the JS and renders a board from scratch
    * Doesn't respond to clicks
    * Has hard-coded initial state to validate rendering

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,24 @@
-const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const pluginWebc = require("@11ty/eleventy-plugin-webc")
+const esbuild = require("esbuild")
 
 const appDir = "app"
+const outDir = "_site"
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc);
 
+  eleventyConfig.addWatchTarget(`./${appDir}/javascripts/`)
+  eleventyConfig.on("eleventy.before", async () => {
+    await esbuild.build({
+      entryPoints: [
+        `./${appDir}/javascripts/vanilla-js.js`,
+      ],
+      bundle: true,
+      outdir: `./${outDir}/javascripts/`,
+    })
+  })
+
   eleventyConfig.addPassthroughCopy(`./${appDir}/stylesheets/`)
-  eleventyConfig.addPassthroughCopy(`./${appDir}/javascripts/`)
 
   return {
     dir: { input: appDir, output: "_site" }
